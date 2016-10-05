0c06906741ec79f8067ed6931bf841e0980f749c
jackie sparling <jmarie9345@gmail.com>
2016-10-05 09:14:54 -0400

    login model

--- app/landing/landingController.js
+++ app/landing/landingController.js
@@ -7,34 +7,35 @@
 
     function LandingController($scope, $rootScope, authorService) {
 
+    	$scope.inputPassword = "";
     	$scope.name = authorService;
         $scope.map = { center: { latitude: 43.033980, longitude: -82.464194 }, zoom: 10 };
     
-$scope.marker = {
-      id: 0,
-      coords: {
-        latitude: 43.033980,
-        longitude: -82.464194 
-      },
-      options: { draggable: true },
-      events: {
-        dragend: function (marker, eventName, args) {
-          $log.log('marker dragend');
-          var lat = marker.getPosition().lat();
-          var lon = marker.getPosition().lng();
-          $log.log(lat);
-          $log.log(lon);
+		$scope.marker = {
+		      id: 0,
+		      coords: {
+		        latitude: 43.033980,
+		        longitude: -82.464194 
+		      },
+		      options: { draggable: true },
+		      events: {
+		        dragend: function (marker, eventName, args) {
+		          $log.log('marker dragend');
+		          var lat = marker.getPosition().lat();
+		          var lon = marker.getPosition().lng();
+		          $log.log(lat);
+		          $log.log(lon);
 
-          $scope.marker.options = {
-            draggable: true,
-            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-            labelAnchor: "100 0",
-            labelClass: "marker-labels"
-          };
-        }
-      }
-    };
-    };
+		          $scope.marker.options = {
+		            draggable: true,
+		            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+		            labelAnchor: "100 0",
+		            labelClass: "marker-labels"
+		          };
+		        }
+		      }
+		    };
+		    };
 
 
 })();
